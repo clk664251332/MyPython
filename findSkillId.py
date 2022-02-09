755724632df9a63a342789df6dfda56ca1002f68
陈龙坤 <664251332@qq.com>
2022-02-09 21:04:19 +0800

    11

--- findSkillId.py
+++ findSkillId.py
@@ -3,7 +3,8 @@ import os
 
 CSV_PATH = "./CSV"
 
-Files = ['./CSV/SkillComboTable.csv']
+#Files = ['./CSV/SkillComboTable.csv']
+Files = []
 ExcludeList = [
     "SkillTable.csv",
     "SkillPetTable.csv",
@@ -26,28 +27,36 @@ def UnpackToLists(value):
 def OnGetValue(value):
     if pd.isna(value):
         return
-    print(value.name)
     list = UnpackToLists(value)
     for i in list:
         for j in i:
             if pd.isna(j):
                 continue
             if(any(skill_ids.str.contains(j))):
-                print("包含")
+                return True
+    return False
 
 def main():
+    f = open("count.txt","w")
     for root, dirs, files in os.walk(CSV_PATH, topdown = True):
         for name in files:
             if name not in ExcludeList and name.endswith(".csv"):
                 Files.append(os.path.join(root, name))
-
+    try:
         for file_path in Files:
+            print("处理 " + file_path)
+            f.writelines('*'*20 + file_path + '*'*20)
             df = pd.read_csv(file_path)
-            df.applymap(OnGetValue)
+            for column in df:
+                for value in df[column]:
+                    ret = OnGetValue(value)
+                    if ret:
+                        f.writelines('column = '+column+"  value = "+value)
+    except Exception as e:
+        print("报错： "+ str(e))
+        f.close()
+    else:
+        f.close()
 
-def Test():
-    for file_path in Files:
-        df = pd.read_csv(file_path)
-        df.applymap(OnGetValue) 
-
-Test()
+    os.system("pause")
+main()
