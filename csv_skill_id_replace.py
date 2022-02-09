76df4f6a1fb07f46e5f3ce1d698230e6cee1fc83
陈龙坤 <664251332@qq.com>
2022-02-09 17:57:37 +0800

    修改

--- csv_skill_id_replace.py
+++ csv_skill_id_replace.py
@@ -4,6 +4,8 @@ import os
 
 CSV_PATH = "./CSV"
 CSV_NEW_PATH = "./NewCSV/"
+CREATE_NEW_FILE = False
+Skill_Ids = None
 
 '''def VectorToTable(value):
     vector = value.split('|')
@@ -20,12 +22,15 @@ def PackToStr(value):
     Str = ""
     for i in range(len(value)):
         sequence = value[i]
-        if not i == 0 and not i == len(value)-1:
-            Str = Str + '|'
         for j in range(len(sequence)):
+            if pd.isna(sequence[j]):
+                continue
             Str = Str + str(sequence[j])
             if not j == len(sequence)-1:
                 Str = Str + '='
+        if not i == len(value)-1:
+            Str = Str + '|'
+
     return Str
 
 #============================替换函数 Start============================
@@ -54,7 +59,7 @@ def replace_id_iv(value, args):
         for j in range(len(element)):
             if j == skillIdIndex:
                 #print("before= ", element[skillIdIndex])
-                lv = element[skillLvIndex] if not skillLvIndex == None and len(element)-1 > skillLvIndex else 1
+                lv = element[skillLvIndex] if not skillLvIndex == None and len(element) > skillLvIndex else 1
                 element[skillIdIndex] = pd.to_numeric(element[skillIdIndex])*100 + pd.to_numeric(lv)
                 #print("after= ", element[skillIdIndex])
     return PackToStr(lists)
@@ -66,6 +71,18 @@ def replace_task(value):
         lists[1] = lists[1] * 100 + 1
     return PackToStr(lists)
 
+#每个字符串与技能id一一匹配，如果匹配到则更改技能id（有风险，更改完需要再次确认下是否替换了应该替换的）
+def replace_match_id(value):
+    lists = UnpackToLists(value)
+    for i in range(len(lists)):
+        element = lists[i]
+        for j in range(len(element)):
+            try:
+                if any((Skill_Ids.str.contains(element[j]))):
+                    element[j] = pd.to_numeric(element[j])*100 + 1
+            except:
+                continue
+        return PackToStr(lists)
 
 def Todo(value):
     return value
@@ -75,22 +92,17 @@ CSV_CONFIG = [
     ["EntityTable", ["UnitSkillID"], [replace_id_iv], [[0,1]]],
     ["EquipReformSkillTable", ["SkillID"], [replace_one_id]],
     ["SkillComboTable", ["Combo","ReplaceSkillId"], [replace_all, replace_one_id]],
-    ["TaskTable/TaskTable", ["Target1","Target2","Target3","Target4","Target5"], [replace_task]],
-    ["EntityTable", ["UnitSkillID"], [replace_id_iv], [[0,1]]],
     ["MercenaryTable", ["Skill"], [replace_id_iv], [[0,1]]],
     ["MerchantMakeMaterialsTable", ["Unlock"], [replace_id_iv], [[0,1]]],
-    ["ProfessionTable", ["SkillIds","FixedSkillIds","FixedUnlockSkillIds","FixedCommonSkillIds"], [replace_id_iv], [[0,1]]],
+    ["ProfessionTable", ["SkillIds","RootTransfromSkills","FixedSkillIds","FixedUnlockSkillIds","FixedCommonSkillIds"], [replace_all, replace_all, replace_id_iv], [None, None, [0,1]]],
     ["SlimeTable", ["Skill","DefaultSkill"], [replace_id_iv], [[0,1]]],
     ["MerchantMakeEnchantTable", ["UnlockSkillID"], [replace_id_iv], [[0,1]]],
-    ["EquipReformSkillTable", ["SkillID"], [replace_one_id]],
     ["CareerPreviewTable", ["RecommendedSkills"], [replace_id_iv], [[0,1]]],
     ["AlchemistMake", ["Unlock"], [replace_id_iv], [[0,1]]],
     ["ScrollMakeTable", ["Unlock"], [replace_id_iv], [[0,1]]],
     ["CartRemouldTable", ["UnlockSkillID"], [replace_id_iv], [[0,1]]],
     ["QTEGuideTable", ["SkillId","ShowSkillId"], [replace_one_id]],
     ["QTESkillTable", ["SkillGroup"], [replace_id_iv], [[0,1]]],
-    ["EquipTable", ["EntryAttributeOne","EntryAttributeTwo"], [Todo]],
-    ["SkillComboTable", ["Combo","ReplaceSkillId"], [replace_all, replace_one_id]],
     #----------------------------------task---start-------------------------------------------
     ["TaskTable/TaskTable", ["Target1","Target2","Target3","Target4","Target5"], [replace_task]],
     ["TaskTable/TaskSplitWorldTable", ["Target1","Target2","Target3","Target4","Target5"], [replace_task]],
@@ -119,19 +131,20 @@ CSV_CONFIG = [
     #----------------------------------task---end-------------------------------------------
     ["AutoAddSkilPointDetailTable", ["AddSkillQueue"], [replace_id_iv], [[0,1]]],
     ["FirstComboSkillTable", ["SkillID"], [replace_one_id]],
-    #["GlobalTable", ["Skill"], [Todo]],
-    #["GuildActivityTable", ["Skill"], [VSReplace_id_lv], [[0,1]]],
     ["LeaderMethodSkillTable", ["SkillID"], [replace_one_id]],
     ["LeaderMethodDetailTable", ["SkillIds"], [replace_all]],
     ["MonkQTESkillTable", ["FirstSkillID","SecondSkillID","ThirdSkillID","FourthSkillID","FifthSkillID","SixthSkillID"], [replace_one_id]],
-    ["MonkQTECustomSkillTable", ["SkillID"], [replace_one_id]],
+    ["MonkQTECustomSkillTable", ["SkillID","ContinueSkillID"], [replace_one_id, replace_all]],
     ["OpenSkillTable", ["SkillIds"], [replace_all]],
     ["SkillClassRecommandTable", ["SkillIds"], [replace_all]],
     ["SkillDataTable", ["SkillId"], [replace_one_id]],
     ["SkillMatchTable", ["Skill_Id_0","Skill_Id_1","Skill_Id_2","Skill_Id_3","Skill_Id_4"], [replace_one_id]],
     ["TdUnitTable", ["SummonCosts"], [replace_id_iv], [[3]]],
-    ["EquipHoleTable", ["Property"], [Todo]],
-    ["EquipTable", ["EntryAttributeOne"], [Todo]]
+    #["EquipTable", ["EntryAttributeOne","EntryAttributeTwo"], [Todo]],
+    #["EquipHoleTable", ["Property"], [Todo]],
+    #["GlobalTableFight", ["Value"], [replace_match_id]],
+    #["GlobalTableSystem", ["Value"], [replace_match_id]],
+    #["GuildActivityTable", ["Value"], [replace_match_id]],
 ]
 
 def StartProcess(configRow):
@@ -175,21 +188,28 @@ def StartProcess(configRow):
             if not pd.isna(value) and not value == "0" and not value == "" and not value == " ":
                 result = func(value, params) if not params == None else func(value)
                 data.iloc[rowIndex, columnsIndex] = result
-    
+
+    #空列名还原为空
+    data = data.rename(columns=lambda x: "" if not x.find("Unnamed:") == -1 else x)
     #导出文件
     try:
-        targetFilePath = os.path.join(CSV_NEW_PATH, tableName + "_new" +".csv")
-        targetFolderPath = os.path.dirname(targetFilePath)
-        if not os.path.exists(targetFolderPath):
-            os.makedirs(targetFolderPath)
-        data.to_csv(targetFilePath, index=False)
-    except:
-        print("导出失败：" + targetFilePath)
+        if CREATE_NEW_FILE == True:
+            targetFilePath = os.path.join(CSV_NEW_PATH, tableName + "_new" +".csv")
+            targetFolderPath = os.path.dirname(targetFilePath)
+            if not os.path.exists(targetFolderPath):
+                os.makedirs(targetFolderPath)
+        else:
+            targetFilePath = os.path.join(CSV_PATH, tableName +".csv")
+        data.to_csv(targetFilePath, index=False, encoding="utf_8_sig")
+    except Exception as e:
+        print("导出失败：" + targetFilePath + "     原因：" + str(e))
         return False
     else:
         print("导出成功：" + targetFilePath)
 
 def main():
+    global Skill_Ids
+    Skill_Ids = pd.read_csv(os.path.join(CSV_PATH, "SkillTable.csv"))["Id"]
     for i in range(len(CSV_CONFIG)):
         result = StartProcess(CSV_CONFIG[i])
         if result == False:
