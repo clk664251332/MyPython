6e666c2fc34eb3926cf3694f5443e1f55b3579e3
clk <664251332@qq.com>
2022-08-09 04:06:30 +0800

    采用更好的方法匹配

--- crawler_test.py
+++ crawler_test.py
@@ -10,11 +10,9 @@ headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 }
 
-find_movie_name = re.compile(r"<h2>(.*?)<\/h2>")
-find_actor_name = re.compile(r"<a href=(.*?)>(.*)<\/a> <\/span>")
-
 movie_names = []
 actor_names = []
+pages = []
 
 #请求并返回html数据
 def Request(url):
@@ -24,23 +22,26 @@ def Request(url):
     return html
 
 #从html数据中提取想要的数据
-def ExtractData(html):
+def ExtractData(html, page):
     bs = BeautifulSoup(html, "html.parser")
     t_list = bs.find_all("div", class_="grid-info-inner") #这个class会和python冲突，所以后面加个下划线
     for element in t_list:
         element = str(element)
-        movie_find_result = re.findall(find_movie_name, element)
-        actor_find_result = re.findall(find_actor_name, element)
-        movie_name_result = "NA"
-        actor_name_result = "NA"
-        if len(movie_find_result) == 0 or len(actor_find_result) == 0:
-            pass
-        else:
-            movie_name_result = movie_find_result[0]
-            actor_name_result = actor_find_result[0][1]
+        bs = BeautifulSoup(element, "html.parser")
+        movie_name_result_list = bs.select("div > a > h2")
+        actor_name_result_list = bs.select("div > span > a")
+        movie_name_result = movie_name_result_list[0].get_text()
+
+        actor_name_result = ""
+        for i in range(0, len(actor_name_result_list)):
+            if i != len(actor_name_result_list)-1:
+                actor_name_result = actor_name_result + actor_name_result_list[i].get_text() + " & "
+            else:
+                actor_name_result = actor_name_result + actor_name_result_list[i].get_text()
 
         movie_names.append(movie_name_result)
         actor_names.append(actor_name_result)
+        pages.append(page)
 
         print(actor_name_result + " - " + movie_name_result)
 
@@ -48,12 +49,13 @@ def SaveData():
     data = {
         'Movie':movie_names,
         'Actor':actor_names,
+        'Page':pages
     }
     df = pd.DataFrame(data)
     df.to_csv('StasyQvr演员表.csv', index=False, encoding='utf-8')
 
 
-for i in range(17, 44):
+for i in range(1, 44):
     url = URL.format(i)
     try:
         html = Request(url)
@@ -61,7 +63,7 @@ for i in range(17, 44):
          print(e)
          continue
     print("----第{0}页----".format(i))
-    ExtractData(html)     
+    ExtractData(html, i)     
     SaveData()
 
 os.system("pause")
