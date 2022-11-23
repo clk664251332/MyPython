b3f4bed41fbcd6849ea9f5c315980d73ac32ef1d
陈龙坤 <664251332@qq.com>
2022-11-23 21:12:53 +0800

    完善

--- test.py
+++ test.py
@@ -1,13 +1,29 @@
-from audioop import add
-from operator import truediv
-from pickle import TRUE
-from pydoc import doc
+import urllib
+import urllib.request
+import re
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
 
 
-#print("我叫%s今年%s岁" % ("哈哈哈", "99"))
+URL = r"http://www.youdao.com/w/eng/water"
+headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+}
+PHONETIC_PATTERN = r"\[.*\]"
 
-test = "哈哈\n"
-for i in range(4, 9):
-    test = test + '*'*i + '\n'
+def Request(url):
+    req = urllib.request.Request(url=url, headers=headers, method="GET")
+    response = urllib.request.urlopen(req)
+    html = response.read().decode("utf-8")
+    bs = BeautifulSoup(html, "html.parser")
+    t_list = bs.find_all("div", class_="baav")
+    return t_list[len(t_list) - 1]
 
-print(test)
+
+
+#test = "<span class=\"phonetic\">[ˈwɔːtə(r)]</span>"
+
+string_result = str(Request(URL))
+#result = re.findall(PHONETIC_PATTERN, string_result)
+print(string_result)
--- vocabulary.py
+++ vocabulary.py
@@ -8,12 +8,21 @@
 #后面每一行都是拓展
 #检测到空行 该区域解析完成
 import re
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
 
 SOURCE_FILE = "vocabulary_test.txt"
 TARGET_FILE = "vocabulary_new.txt"
 PATTERN = r"\[[^\u4e00-\u9fa5]*?\]"
+PHONETIC_PATTERN = r"\[.*\]"
 Title_Replace = "*{0}\n**<div style='text-align:center'><h1>{1}</h1></div><div style='text-align:center; font-size:85%;'><span style='font-family: Arial; color:blue;'>{2}</span></div>\n"
 Paraphrase_Replace = '***{0}\n<hr style="height:1px;border:none;border-top:1px dashed #0066CC;background-color:#ffffff;">\n'
+URL = r"http://www.youdao.com/w/eng/{0}"
+HEADERS = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+}
+
 line_count = 0
 all_count = 0
 current_vob = ""
@@ -22,6 +31,38 @@ current_vob = ""
 def Strip(line):
     line = re.sub(PATTERN, "", line)
     return line.strip()
+
+#从网络获取音标
+def RequestPhoneticSymbol(world):
+    reqUrl = URL.format(world)
+    req = urllib.request.Request(url=reqUrl, headers=HEADERS, method="GET")
+    response = urllib.request.urlopen(req)
+    html = response.read().decode("utf-8")
+    bs = BeautifulSoup(html, "html.parser")
+    t_list = bs.find_all("div", class_="baav")
+    if len(t_list) <=0:
+        print(world + " 没有查到音标")
+        return "" 
+    result = str(t_list[len(t_list) - 1])
+    extracted_result = re.findall(PHONETIC_PATTERN, result)
+    length = len(extracted_result)
+    if length>0:
+        return extracted_result[length -1]
+    else:
+        print(result + "，没有提取出音标")
+        return ""
+
+#获取音标
+def GetPhoneticSymbol(word):
+    count = len(str.split(word, ' '))
+    if count > 1: #词组不查找音标
+        return ""
+    else:
+        try:
+            return RequestPhoneticSymbol(word)
+        except urllib.error.URLError as e:
+            print(str.format("网络获取单词{0}音标失败，原因为{1}", word, e))
+            return ""
 
 #对读取的每一行进行处理
 def ProcessLine(line):
@@ -44,12 +85,14 @@ def ProcessLine(line):
 
 #处理标题
 def ProcessTitle(line):
+    print("正在处理 " + line)
     global all_count
     global current_vob
     current_vob = line
     all_count = all_count + 1
     #line = "标题：" + line
-    return Title_Replace.format(line, line, current_vob + "的音标")
+    phonetic_symbol = GetPhoneticSymbol(line)
+    return Title_Replace.format(line, line, phonetic_symbol)
 
 #处理释义
 def ProcessParaphrase(line):
